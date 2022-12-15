3947c7510efd23b3a7202e95c5fc589b8b5a3da3
Alanthomas-2209 <2209@tkmce.ac.in>
2022-12-16 00:24:28 +0530

    bst

--- DS/binary search tree/code/bst.c
+++ DS/binary search tree/code/bst.c
@@ -6,6 +6,8 @@ struct node{
 	struct node *right;
 };
 
+//creating new node
+
 struct node* set(int data){
 	struct node* newnode = (struct node*)malloc(sizeof(struct node));
 	newnode->data=data;
@@ -13,6 +15,7 @@ struct node* set(int data){
 	return newnode;
 }
 
+//insertion
 
 struct node* insertion(struct node* newnode,int key){
 	if (newnode == NULL){
@@ -27,15 +30,18 @@ struct node* insertion(struct node* newnode,int key){
 	return newnode;
 }
 
+//display root node
 
 void display(struct node *root){
 	if(root==NULL){
 	printf("Tree is empty....!");
 	}
 	else{
-	printf("Root node: %d",root->data);
+	printf("Root node: %d \n",root->data);
 	}	
 }
+
+//traversal
 
 void preorder(struct node *root){
 	if(root!=NULL){
@@ -59,81 +65,108 @@ void postorder(struct node *root){
 	}
 }
 
-/*
+/* Given a non-empty binary search tree, return the node
+with minimum key value found in that tree. Note that the
+entire tree does not need to be searched. */
+
+struct node *minValueNode(struct node *node){
+	struct node *current = node;
+	while (current && current->left != NULL)
+	current = current->left;
+	return current;
+}
+
+//Deletion
+
 struct node *deleteNode(struct node *root, int key){
 	if (root == NULL) 
 		return root;
-	if (key < root->key)
+	if (key < root->data)
 		root->left = deleteNode(root->left, key);
-  	else if (key > root->key)
+  	else if (key > root->data)
     		root->right = deleteNode(root->right, key);
 
   	else{
+/* 		node has no child
+  		if (root->left == NULL and root->right == NULL)
+            		return NULL;         	
+            	node with only one child or no child */
     		if (root->left == NULL) {
-      		struct node *temp = root->right;
-      		free(root);
-      		return temp;
+      			struct node *temp = root->right;
+      			free(root);
+      			return temp;
     		} else if (root->right == NULL) {
-      		struct node *temp = root->left;
-      		free(root);
-      		return temp;
+      			struct node *temp = root->left;
+      			free(root);
+      			return temp;
     		}
-    	
+    	// node with two children: Get the inorder successor (smallest in the right subtree)
    	struct node *temp = minValueNode(root->right);
-   	root->key = temp->key;
-   	root->right = deleteNode(root->right, temp->key);
+   	root->data = temp->data;
+   	// Delete the inorder successor
+   	root->right = deleteNode(root->right, temp->data);
    	}
-   	
-  return root;
+   	return root;
 }
 
-struct node *minValueNode(struct node *node) {
-  struct node *current = node;
-  while (current && current->left != NULL)
-    current = current->left;
+//main function
 
-  return current;
-}
-*/
 void main(){
 
 	struct node* root = NULL;
-	int choice,data;
+	int choice,data,i,n;
+	printf("OPTIONS:\n1.Creation of binary search tree \n2.Insertion of node\n3.Preorder traverse\n4.Inorder traverse\n5.Post order traverse \n6.Dispaly root node\n7.Deletion of node\n0.Exit\n");
 	do{
 	
-		printf("Enter a choice : \n1.insertion of node\n2.Preorder traverse\n3.Inorder traverse\n4.Post order traverse \n5.Dispaly root node\n0.Exit\n\n");
+		printf("\nEnter your choice :\n");
 		scanf("%d",&choice);
 
 		switch(choice){
-			case 1 : {
+		
+			case 1 : {	
+					printf("How many data want to insert :");
+					scanf("%d",&n);
+					for(i=1;i<n;i++){
+						printf("Enter the data :");
+				    		scanf("%d",&data);
+						root=insertion(root,data);
+					}
+				 } 
+			case 2 : {
 					printf("Enter the data :");
 				    	scanf("%d",&data);
-					insertion(root,data);
-					break;
-				 }
-			case 2 : {
-					preorder(root);
+					root=insertion(root,data);
 					break;
 				 }
 			case 3 : {
-					 inorder(root);
-					 break;
+					preorder(root);
+					break;
 				 }
 			case 4 : {
-					 postorder(root);
-					 break;
+					inorder(root);
+					break;
 				 }
 			case 5 : {
-					 display(root);
-					 break;
+					postorder(root);
+					break;
+				 }
+			case 6 : {
+					display(root);
+					break;
+				 }
+			case 7 : {
+					printf("Enter the data :");
+				    	scanf("%d",&data);
+					root = deleteNode(root,data);
+					break;
 				 }
 			case 0 : {
 					printf("Exiting....!\n");
 					break;
 				 }
 			default : {
-					  printf("Wrong input\n");
-					  break;
+					printf("Wrong input\n");
+					break;
 				  }
 		}
 	}while(choice != 0 );
